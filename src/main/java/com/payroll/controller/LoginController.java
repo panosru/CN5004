4907d75d25bec42dd1c8586f5df292e5845db72b
Panagiotis <400362+panosru@users.noreply.github.com>
2022-03-31 17:59:17 +0300

    Login and Main Controller interaction

--- src/main/java/com/payroll/controller/LoginController.java
+++ src/main/java/com/payroll/controller/LoginController.java
@@ -1,4 +1,46 @@
 package com.payroll.controller;
 
+import com.payroll.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class LoginController
-{}
+{
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private Label errorMessage;
+
+    public void initialize()
+    { }
+
+    public void userLogin(ActionEvent event)
+        throws IOException
+    {
+        if (username.getText().equals("panosru") && password.getText().equals("123"))
+        {
+            App.loadScene("view/main.fxml");
+        }
+        else if (username.getText().isEmpty() || password.getText().isEmpty())
+        {
+            errorMessage.setText("Fill the data");
+        }
+        else
+        {
+            errorMessage.setText("Something went wrong!");
+        }
+    }
+}
--- src/main/java/com/payroll/controller/MainController.java
+++ src/main/java/com/payroll/controller/MainController.java
@@ -1,9 +1,19 @@
 package com.payroll.controller;
 
+import com.payroll.App;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class MainController
 {
     public void initialize()
     {
     }
 
+    public void logout(ActionEvent event)
+        throws IOException
+    {
+        App.loadScene("view/login.fxml");
+    }
 }
