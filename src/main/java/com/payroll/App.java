1160a630ad3b33d954215a648c6267406e370e0a
Panagiotis <400362+panosru@users.noreply.github.com>
2022-03-31 17:58:06 +0300

    Static App scene loader

--- src/main/java/com/payroll/App.java
+++ src/main/java/com/payroll/App.java
@@ -3,34 +3,47 @@ package com.payroll;
 import com.payroll.common.Utils;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class App
     extends Application
 {
+    /**
+     * Scene placeholder
+     */
+    private static Stage stage;
+
     @Override
-    public void start(Stage stage)
+    public void start(Stage primaryStage)
         throws Exception
     {
+        stage = primaryStage;
+
         if (!(Utils.isWindows() || Utils.isMac()))
         {
             //Load Arial font for all *nix platforms
             Font.loadFont(
                 Objects.requireNonNull(
-                    this.getClass().getClassLoader().getResource("fonts/Arial.ttf")).toExternalForm(), 12);
+                    getClass().getClassLoader().getResource("fonts/Arial.ttf")).toExternalForm(), 12);
         }
 
-        FXMLLoader fxmlLoader = new FXMLLoader(
-            this.getClass().getResource("view/login.fxml"));
-
         stage.setTitle(Utils.TITLE);
-        stage.setScene(new Scene(fxmlLoader.load(), Utils.APP_WIDTH, Utils.APP_HEIGHT));
         stage.setResizable(false);
+        loadScene("view/login.fxml");
         stage.show();
+    }
+
+    public static void loadScene(String fxml)
+        throws IOException
+    {
+        Parent pane = FXMLLoader.load(Objects.requireNonNull(App.class.getResource(fxml)));
+        stage.setScene(new Scene(pane, Utils.APP_WIDTH, Utils.APP_HEIGHT));
     }
 
     public static void main(String[] args)
