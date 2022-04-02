806934f642da801af5354a9865dd1b3a65dcf24d
Panagiotis <400362+panosru@users.noreply.github.com>
2022-04-02 15:27:36 +0300

    Added CookieService

---
+++ src/main/java/com/payroll/service/CookieService.java
@@ -0,0 +1,43 @@
+package com.payroll.service;
+
+import com.payroll.App;
+
+import java.util.prefs.Preferences;
+
+public final class CookieService
+{
+    // Making safe the use of same instance via multiple threads at the same time
+    private static volatile CookieService instance;
+
+    private final Preferences store;
+
+    private CookieService()
+    {
+        // Prevent instantiation
+        store = Preferences.userRoot().node(App.class.getName());
+    }
+
+    /**
+     * Thread-safe singleton implementation
+     * @return the instance
+     */
+    public static CookieService getInstance()
+    {
+        CookieService localInstance = instance;
+        // Thread-safe singleton
+        if (null == localInstance)
+            synchronized (CookieService.class)
+            {
+                localInstance = instance;
+                if (null == instance)
+                    instance = localInstance = new CookieService();
+            }
+
+        return localInstance;
+    }
+
+    public Preferences getStore()
+    {
+        return store;
+    }
+}
