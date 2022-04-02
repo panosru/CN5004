d15bbae0a24fbd3146f3d0c90b24f4bbe9e99a6f
Panagiotis <400362+panosru@users.noreply.github.com>
2022-04-02 15:22:30 +0300

    Added hashing methods to Utils

--- src/main/java/com/payroll/common/Utils.java
+++ src/main/java/com/payroll/common/Utils.java
@@ -1,5 +1,6 @@
 package com.payroll.common;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -84,5 +85,31 @@ public final class Utils
     public static @NotNull String normaliseUpper(String s)
     {
         return normaliseLower(s).toUpperCase();
+    }
+
+    /**
+     * This method is used to generate a string representing a hash of a string (e.g. password)
+     * suitable for storing in a database. It will be an OpenBSD-style crypt(3) formatted
+     * hash string of length=60
+     * A workload of 12 is a very reasonable safe default as of 2013.
+     * This automatically handles secure 128-bit salt generation and storage within the hash.
+     * @param s The plaintext string to hash.
+     * @return String - a string of length 60 that is the bcrypt hashed string in crypt(3) format.
+     */
+    public static String hashString(String s) {
+        // Define the BCrypt workload to use when generating password hashes. 10-31 is a valid value.
+        return BCrypt.withDefaults().hashToString(12, s.toCharArray());
+    }
+
+    /**
+     * This method can be used to verify a computed hash from a plaintext (e.g. during a login
+     * request) with that of a stored hash from a database. The hash from the database
+     * must be passed as the second variable.
+     * @param s The plaintext string.
+     * @param stored_hash The stored string hash, retrieved from the database.
+     * @return boolean - true if the string matches the string of the stored hash, false otherwise
+     */
+    public static boolean verifyHash(String s, String stored_hash) {
+        return (BCrypt.verifyer().verify(s.toCharArray(), stored_hash)).verified;
     }
 }
