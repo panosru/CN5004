535ddd3624f3d00e292956d45d3ce6334f773ffa
Panagiotis <400362+panosru@users.noreply.github.com>
2022-04-03 20:41:40 +0300

    Close button is moved to shared components

--- src/main/java/com/payroll/controller/BaseController.java
+++ src/main/java/com/payroll/controller/BaseController.java
@@ -1,26 +1,13 @@
 package com.payroll.controller;
 
 import com.payroll.App;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 
 public abstract class BaseController
 {
     @FXML
-    protected Button closeBtn;
-
-    @FXML
     private Pane rootPane;
-
-    public void closeApp(ActionEvent actionEvent)
-    {
-        App.getStage().close(); // close the application
-        Platform.exit(); // close GUI thread
-        System.exit(0); // close JVM | 0 = normal exit
-    }
 
     public void initialize()
     {
