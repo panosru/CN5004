bded0b099a89feb4347fbe4ca36289b88796666b
Panagiotis <400362+panosru@users.noreply.github.com>
2022-04-03 19:19:57 +0300

    Main controller sidebar

--- src/main/java/com/payroll/controller/MainController.java
+++ src/main/java/com/payroll/controller/MainController.java
@@ -2,11 +2,16 @@ package com.payroll.controller;
 
 import com.payroll.App;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import org.controlsfx.control.BreadCrumbBar;
 
 import java.io.IOException;
 
 public class MainController extends BaseController
 {
+    @FXML
+    private BreadCrumbBar<String> bread;
+
     public void logout(ActionEvent event)
         throws IOException
     {
