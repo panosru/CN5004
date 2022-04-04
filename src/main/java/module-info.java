212a2ec22fcfa511dae3ec99ebf5ccab15474502
Panagiotis <400362+panosru@users.noreply.github.com>
2022-04-04 17:23:55 +0300

    improved dependencies and fixed module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module com.cn5004ap.payroll {
+    requires java.prefs;
+    requires javafx.base;
+    requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.prefs;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
     requires jakarta.persistence;
     requires org.jetbrains.annotations;
     requires org.hibernate.orm.core;
@@ -9,6 +14,7 @@ module com.cn5004ap.payroll {
 
     opens com.cn5004ap.payroll.controller to javafx.fxml;
     opens com.cn5004ap.payroll.persistence to org.hibernate.orm.core;
+    opens com.cn5004ap.payroll.view to org.kordamp.ikonli.javafx;
 
     exports com.cn5004ap.payroll;
     exports com.cn5004ap.payroll.controller;
