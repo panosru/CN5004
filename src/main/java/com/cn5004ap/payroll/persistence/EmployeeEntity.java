c152cf6c9800bb3db24b1bdf1edd86b0aeddcefc
Panagiotis <400362+panosru@users.noreply.github.com>
2022-06-05 12:17:27 +0300

    Updated EmployeeEntity

--- src/main/java/com/cn5004ap/payroll/persistence/EmployeeEntity.java
+++ src/main/java/com/cn5004ap/payroll/persistence/EmployeeEntity.java
@@ -173,14 +173,184 @@ public class EmployeeEntity
         this.lastName = lastName;
     }
 
-    public Date getTerminationDate()
+    public String getFullName(boolean reverse)
     {
-        return terminationDate;
+        String[] fullName = {getFirstName(), getLastName()};
+
+        if (reverse)
+            Collections.reverse(Arrays.asList(fullName));
+
+        return String.join(" ", fullName);
     }
 
-    public void setTerminationDate(Date terminationDate)
+    public String getFullName()
     {
-        this.terminationDate = terminationDate;
+        return getFullName(false);
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public Date getBirthDate()
+    {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate)
+    {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
+
+    public void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+
+    public Gender getGenderEnum()
+    {
+        return getGender().equals("F") ? Gender.FEMALE : Gender.MALE;
+    }
+
+    public String getIban()
+    {
+        return iban;
+    }
+
+    public void setIban(String iban)
+    {
+        this.iban = iban;
+    }
+
+    public String getSsn()
+    {
+        return ssn;
+    }
+
+    public void setSsn(String ssn)
+    {
+        this.ssn = ssn;
+    }
+
+    public String getDepartment()
+    {
+        return department;
+    }
+
+    public void setDepartment(String department)
+    {
+        this.department = department;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public double getGrossSalary()
+    {
+        return grossSalary;
+    }
+
+    public void setGrossSalary(double salary)
+    {
+        this.grossSalary = salary;
+    }
+
+    public double getInsuranceExpense()
+    {
+        return getInsuranceExpense(getGrossSalary());
+    }
+
+    public static double getInsuranceExpense(double grossSalary)
+    {
+        return grossSalary * (SettingsController.getInsurancePercent() / 100);
+    }
+
+    public double getTaxExpense()
+    {
+        return getTaxExpense(getGrossSalary());
+    }
+
+    public static double getTaxExpense(double grossSalary)
+    {
+        return grossSalary * (SettingsController.getTaxPercent() / 100);
+    }
+
+    public double getNetSalary()
+    {
+        return getNetSalary(getGrossSalary(), getInsuranceExpense(), getTaxExpense());
+    }
+
+    public static double getNetSalary(double grossSalary, double insurance, double tax)
+    {
+        return grossSalary - insurance - tax;
+    }
+
+    public boolean isActive()
+    {
+        return active;
+    }
+
+    public void setActive(boolean active)
+    {
+        this.active = active;
+    }
+
+    public Status getStatus()
+    {
+        return isActive() ? Status.ACTIVE : Status.INACTIVE;
+    }
+
+    public String getStatusUnicode()
+    {
+        return isActive() ? "\u2713" : "\u2718";
+    }
+
+    public Date getEmploymentDate()
+    {
+        return employmentDate;
+    }
+
+    public void setEmploymentDate(Date employmentDate)
+    {
+        this.employmentDate = employmentDate;
     }
 
     public Period getEmploymentPeriod()
@@ -189,6 +359,16 @@ public class EmployeeEntity
             Utils.convertDateToLocal(getEmploymentDate()),
             Utils.convertDateToLocal(isActive() ? new Date() : getTerminationDate())
         );
+    }
+
+    public Date getTerminationDate()
+    {
+        return terminationDate;
+    }
+
+    public void setTerminationDate(Date terminationDate)
+    {
+        this.terminationDate = terminationDate;
     }
 
     public Period getTerminationPeriod()
@@ -220,184 +400,6 @@ public class EmployeeEntity
     public void setLastUpdated(Date lastUpdated)
     {
         this.lastUpdated = lastUpdated;
-    }
-
-    public String getAddress()
-    {
-        return address;
-    }
-
-    public void setAddress(String address)
-    {
-        this.address = address;
-    }
-
-    public String getIban()
-    {
-        return iban;
-    }
-
-    public void setIban(String iban)
-    {
-        this.iban = iban;
-    }
-
-    public String getSsn()
-    {
-        return ssn;
-    }
-
-    public void setSsn(String ssn)
-    {
-        this.ssn = ssn;
-    }
-
-    public String getPhone()
-    {
-        return phone;
-    }
-
-    public void setPhone(String phone)
-    {
-        this.phone = phone;
-    }
-
-    public Date getEmploymentDate()
-    {
-        return employmentDate;
-    }
-
-    public void setEmploymentDate(Date employmentDate)
-    {
-        this.employmentDate = employmentDate;
-    }
-
-    public String getFullName(boolean reverse)
-    {
-        String[] fullName = {getFirstName(), getLastName()};
-
-        if (reverse)
-            Collections.reverse(Arrays.asList(fullName));
-
-        return String.join(" ", fullName);
-    }
-
-    public String getFullName()
-    {
-        return getFullName(false);
-    }
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-
-    public String getDepartment()
-    {
-        return department;
-    }
-
-    public void setDepartment(String department)
-    {
-        this.department = department;
-    }
-
-    public String getTitle()
-    {
-        return title;
-    }
-
-    public void setTitle(String title)
-    {
-        this.title = title;
-    }
-
-    public double getGrossSalary()
-    {
-        return grossSalary;
-    }
-
-    public double getInsuranceExpense()
-    {
-        return getGrossSalary() * (SettingsController.getInsurancePercent() / 100);
-    }
-
-    public double getTaxExpense()
-    {
-        return getGrossSalary() * (SettingsController.getTaxPercent() / 100);
-    }
-
-    public double getNetSalary()
-    {
-        return getGrossSalary() - getInsuranceExpense() - getTaxExpense();
-    }
-
-    public void setGrossSalary(double salary)
-    {
-        this.grossSalary = salary;
-    }
-
-    public Date getBirthDate()
-    {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate)
-    {
-        this.birthDate = birthDate;
-    }
-
-    public String getGender()
-    {
-        return gender;
-    }
-
-    public void setGender(String gender)
-    {
-        this.gender = gender;
-    }
-
-    public Gender getGenderEnum()
-    {
-        return getGender().equals("F") ? Gender.FEMALE : Gender.MALE;
-    }
-
-    public boolean isActive()
-    {
-        return active;
-    }
-
-    public void setActive(boolean active)
-    {
-        this.active = active;
-    }
-
-    public Status getStatus()
-    {
-        return isActive() ? Status.ACTIVE : Status.INACTIVE;
-    }
-
-    public String getStatusUnicode()
-    {
-        return isActive() ? "\u2713" : "\u2718";
-    }
-
-
-    public static String salaryPrettify(double salary)
-    {
-        return salaryPrettify(salary, 2);
-    }
-
-    public static String salaryPrettify(double salary, int decimals)
-    {
-        NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.GERMANY);
-        formatter.setMaximumFractionDigits(decimals);
-        return formatter.format(salary);
     }
 
     @Override
